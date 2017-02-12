3b9e081fd381a318c87bb63268d1afe610ac9cda
Damith <damithme@gmail.com>
2017-02-12 19:57:20 +0530

    add code

---
+++ api/src/main/java/com/company/api/APIService.java
@@ -0,0 +1,15 @@
+package com.company.api;
+
+
+import com.company.core.CoreService;
+
+/**
+ * Created by DAM on 2/10/17.
+ */
+public class APIService {
+
+    public void apiService(){
+        CoreService service = new CoreService();
+        service.getCoreService();
+    }
+}
---
+++ core/src/main/java/com/company/core/CoreService.java
@@ -0,0 +1,12 @@
+package com.company.core;
+
+/**
+ * Created by DAM on 2/10/17.
+ */
+public class CoreService {
+
+    public void getCoreService() {
+        System.out.println("Core Service");
+    }
+
+}
